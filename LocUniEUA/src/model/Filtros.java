151a6d0c68deccaca2d4c97aee62852e56e7ba30
ignaciojvig <ignaciojvig@gmail.com>
2016-10-14 12:05:44 -0300

    Atualização Geral do Sistema

---
+++ LocUniEUA/src/model/Filtros.java
@@ -0,0 +1,35 @@
+package model;
+
+/**
+ *
+ * @author JoãoVictor
+ * Comentário por Luís
+ * Comentário por Karina
+ * Olá
+ */
+public class Filtros {
+    String tiposelecao = "";
+    String tipoparametro = "";
+    
+    void gerarResultado(){
+        //Gera os gráficos
+    }
+
+    public String getTiposelecao() {
+        return tiposelecao;
+    }
+
+    public void setTiposelecao(String tiposelecao) {
+        this.tiposelecao = tiposelecao;
+    }
+
+    public String getTipoparametro() {
+        return tipoparametro;
+    }
+
+    public void setTipoparametro(String tipoparametro) {
+        this.tipoparametro = tipoparametro;
+    }
+    
+    
+}
